22c296ae8c7cdf4f69fb523a5c4f1ebf63e08ada
Gehan1226 <gehansithija2023@gmail.com>
2024-07-22 18:20:43 +0530

    update single paterns

--- src/main/java/org/example/bo/BoFactory.java
+++ src/main/java/org/example/bo/BoFactory.java
@@ -1,27 +1,39 @@
 package org.example.bo;
 
 import org.example.bo.custom.impl.*;
-import org.example.dao.custom.impl.ItemDaoImpl;
 import org.example.util.BoType;
 
 public class BoFactory {
     private static BoFactory instance;
-    private BoFactory(){}
 
-    public static BoFactory getInstance(){
-        return instance != null ? instance:(instance = new BoFactory());
+    private BoFactory() {
     }
-    public <T extends SuperBo>T getBo(BoType type){
-        switch (type){
-            case EMPLOYEE:return (T) new EmployeeBoImpl();
-            case USER:return (T) new UserBoImpl();
-            case VALIDATE:return (T) new DataValidationBoImpl();
-            case ITEM:return (T) new ItemBoImpl();
-            case SUPPLIER:return (T) new SupplierBoImpl();
-            case ORDER:return (T) new OrderBoImpl();
-            case SALES:return (T) new SalesReportBoImpl();
+
+    public static BoFactory getInstance() {
+        if (instance != null) return instance;
+        instance = new BoFactory();
+        return instance;
+    }
+
+    public <T extends SuperBo> T getBo(BoType type) {
+        switch (type) {
+            case EMPLOYEE:
+                return (T) new EmployeeBoImpl();
+            case USER:
+                return (T) new UserBoImpl();
+            case VALIDATE:
+                return (T) new DataValidationBoImpl();
+            case ITEM:
+                return (T) new ItemBoImpl();
+            case SUPPLIER:
+                return (T) new SupplierBoImpl();
+            case ORDER:
+                return (T) new OrderBoImpl();
+            case SALES:
+                return (T) new SalesReportBoImpl();
+            default:
+                throw new IllegalArgumentException("Invalid BoType: " + type);
         }
-        return null;
     }
 }
 
--- src/main/java/org/example/dao/Daofactory.java
+++ src/main/java/org/example/dao/Daofactory.java
@@ -6,19 +6,30 @@ import org.example.util.DaoType;
 public class Daofactory {
 
     private static Daofactory instance;
-    private Daofactory(){}
 
-    public static Daofactory getInstance(){
-        return instance != null ? instance: (instance = new Daofactory());
+    private Daofactory() {
     }
-    public <T extends SuperDao>T getDao(DaoType type){
-        switch (type){
-            case USER:return (T) new UserDaoImpl();
-            case EMPLOYEE:return (T) new EmployeeDaoImpl();
-            case ITEM:return (T) new ItemDaoImpl();
-            case SUPPLIER:return (T) new SupplierDaoImpl();
-            case ORDER:return (T) new OrderDaoImpl();
+
+    public static Daofactory getInstance() {
+        if (instance != null) return instance;
+        instance = new Daofactory();
+        return instance;
+    }
+
+    public <T extends SuperDao> T getDao(DaoType type) {
+        switch (type) {
+            case USER:
+                return (T) new UserDaoImpl();
+            case EMPLOYEE:
+                return (T) new EmployeeDaoImpl();
+            case ITEM:
+                return (T) new ItemDaoImpl();
+            case SUPPLIER:
+                return (T) new SupplierDaoImpl();
+            case ORDER:
+                return (T) new OrderDaoImpl();
+            default:
+                throw new IllegalArgumentException("Invalid DoType: " + type);
         }
-        return null;
     }
 }
